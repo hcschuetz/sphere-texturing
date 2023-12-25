79fbf55202ddb68b1fa0a7de1bde43ae4a25f67c
Heribert Schütz <hs@webxcerpt.com>
2023-12-25 21:46:53 +0100

    rewrote subdivide

--- src/utils.ts
+++ src/utils.ts
@@ -5,14 +5,12 @@ export const TAU = 2 * Math.PI;
 /** Division avoiding `NaN` for `0 / 0` */
 export const frac = (x: number, y: number) => x === 0 ? 0 : x / y;
 
-export function subdivide(from: number, to: number, nSteps: number): number[] {
-  const result: number[] = [];
-  for (let i = 0, j = nSteps; j >= 0; i++, j--) {
-    result.push(frac((j * from + i * to), nSteps));
-  }
-  return result;
-}
+export const subdivide = (from: number, to: number, nSteps: number): number[] =>
+  Array.from({length: nSteps + 1}, (_, i) =>
+    frac(((nSteps - i) * from + i * to), nSteps)
+  );
 
+// Why isn't there a Vector3.Slerp(...) in babylon.js?
 export const slerp = (from: Vector3, to: Vector3, lambda: number) =>
   Vector3.SlerpToRef(from, to, lambda, new Vector3());
 
