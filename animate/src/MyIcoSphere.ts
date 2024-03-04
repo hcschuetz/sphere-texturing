f6e03deb98d79bb02a4bd19b1f67d56537c39272
Heribert Schütz <hs@webxcerpt.com>
2024-03-04 12:56:55 +0100

    first version of transformation explanation

---
+++ animate/src/MyIcoSphere.ts
@@ -0,0 +1,275 @@
+import * as B from "@babylonjs/core";
+
+// -----------------------------------------------------------------------------
+// General utilities
+
+/** perimeter/radius ratio of a circle */
+const TAU = 2 * Math.PI;
+
+type V3 = B.Vector3;
+const V3 = B.Vector3;
+const v3 = (x: number, y: number, z: number) => new V3(x, y, z);
+
+/**
+ * Create a BABYLON Matrix representing a 3x3 matrix with the given fields.
+ * 
+ * (BABYLON seems to support only 4x4 matrices)
+ */
+const createMat3 = (
+  a: number, b: number, c: number,
+  d: number, e: number, f: number,
+  g: number, h: number, i: number,
+): B.Matrix => B.Matrix.FromValues(
+  a, b, c, 0,
+  d, e, f, 0,
+  g, h, i, 0,
+  0, 0, 0, 1,
+);
+
+// -----------------------------------------------------------------------------
+
+// Sprite Sheet (see also ../MyIcoSphere.md)
+// =========================================
+//
+// 1           -- |---X-------X-------X-------X-------X---|
+// 1   - dv    -- |19/.\ 15  /.\ 16  /.\ 17  /.\ 18  /.\19|           upper
+//                | // \\   // \\   // \\   // \\   // \\ |   polar
+//                |//   \\ //   \\ //   \\ //   \\ //   \\|           lower
+// 1/2 + dv/2  -- '/  0  \'/  1  \'/  2  \'/  3  \'/  4  \'
+// 1/2 - dv/2  -- X-------X-------X-------X-------X-------X
+//                |\  5  / \  6  / \  7  / \  8  / \  9  /|           upper
+//                | \   /   \   /   \   /   \   /   \   / |   equatorial
+//                |14\ / 10  \ / 11  \ / 12  \ / 13  \ /14|           lower
+// 0           -- |---X-------X-------X-------X-------X---|
+//
+// ^         u    |   |   |   |   |   |   |   |   |   |   |
+// |v       ---> 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+//
+//                  l   r   l   r   l   r   l   r   l   r     (left/right)
+//
+// (The distinctions between
+// - polar and equatorial,
+// - upper and lower,
+// - left and right
+// regions are made in the texture conversion code below
+// to figure out to which face a given (u, v) coordinate pair belongs.)
+//
+//
+// Conventions
+// ===========
+//
+// In names and comments we consider the y axis pointing northward.
+// Thus (0, 1, 0) and (0, -1, 0) are the north pole and the south pole of
+// the (unit) sphere and the x/z plane is the equatorial plane.
+//
+// The inscribed icosahedron has vertices at the north and south pole.
+// The upper vertices of faces 1 to 4 in the sprite sheet
+// are mapped to the north pole,
+// the lower vertices of faces 15 to 19 are mapped to the south pole.
+//
+// The left edge of face 0 and the right edge of face 4 are adjacent and mapped
+// to the meridian in the x/y plane, actually in the half plane with x >= 0.
+// Also the central vertical lines in faces 14 and 19 (where they are split in
+// the sprite sheet) are mapped to that meridian.
+//
+// Notice that each icosahedron face has one edge in west-east direction
+// (i.e., parallel to the u axis in the sprite sheet).
+// The opposite corner points to (or even is) the north pole or the south pole
+// (i.e., in the direction of the positive or negative v axis).
+// We use the names
+// - W and E (for "west" and "east") for the two ends of the west-east edge and
+// - P for the "poleward" vertex
+// when dealing with a given face.
+
+// -----------------------------------------------------------------------------
+
+/** How far above/below the equator are the non-pole vertices? */
+const height = Math.sqrt(1 / 5);
+/** How far away from the main axis are the non-pole vertices? */
+const radius = 2 * height;
+
+/**
+ * Vertical "safety gap" between neighboring sprite regions
+ */
+export const dv = .01;
+
+// -----------------------------------------------------------------------------
+// Triangulation
+
+const northPole = v3(0, 1, 0);
+const southPole = v3(0, -1, 0);
+
+// The following calculations only require basic arithmetics and square roots.
+// But trigonometric functions are easier to understand here:
+const nonPoles = Array.from({ length: 10 }, (_, i) => v3(
+  radius * Math.cos(TAU / 10 * i),
+  height * (-1) ** i,
+  radius * Math.sin(TAU / 10 * i)
+));
+// support some circular access:
+nonPoles.push(...nonPoles.slice(0, 2));
+
+/**
+ * Compute vertex data for an icosphere where each edge is subdivided
+ * into `nSteps` segments.
+ */
+export const createIcoVertices = (nSteps: number, bulge = 1) => {
+  const positions: number[] = [];
+  const uvs: number[] = [];
+  let idx = 0;
+
+  function emitVertex(pos: V3, u: number, v: number) {
+    positions.push(...pos.asArray());
+    uvs.push(u, v);
+  }
+
+  const indices: number[] = [];
+
+  function emitIcoFace(w: V3, p: V3, e: V3, u_w: number, v_we: number, v_p: number, flip: boolean): void {
+    function emitTriangle(a: number, b: number, c: number): void {
+      indices.push(a, flip ? c : b, flip ? b : c);
+    }
+
+    for (let i = 0, jk = nSteps; jk >= 0; i++, jk--) {
+      for (let j = 0, k = jk; k >= 0; j++, k--) {
+        const posFace = p.scale(i).addInPlace(e.scale(j)).addInPlace(w.scale(k)).scale(1 / nSteps);
+        const posSphere = posFace.clone().normalize();
+        emitVertex(
+          V3.Lerp(posFace, posSphere, bulge),
+          u_w + (i * .1 + j * .2) / nSteps,
+          (i * v_p + jk * v_we) / nSteps,
+        );
+
+        if (i > 0) {
+          const prevLeft = idx - jk - 2;
+          emitTriangle(idx, prevLeft, prevLeft + 1);
+          if (j > 0) {
+            emitTriangle(idx, idx - 1, prevLeft);
+          }
+        }
+        idx++;
+      }
+    }
+  };
+
+  for (let i = 0; i < 5; i++) {
+    const [upper0, lower1, upper2, lower3] = nonPoles.slice(2*i, 2*i + 4);
+
+    //          w       p          e       u_w       v_we      v_p       flip
+    emitIcoFace(upper0, northPole, upper2,  i    /5, (1-dv)/2,  1-dv   , false);
+    emitIcoFace(upper0, lower1   , upper2,  i    /5, (1-dv)/2,  0      , true );
+    emitIcoFace(lower1, upper2   , lower3, (i+.5)/5,  0      , (1-dv)/2, false);
+    emitIcoFace(lower1, southPole, lower3, (i+.5)/5,  1      , (1+dv)/2, true );
+  };
+
+  return Object.assign(new B.VertexData(), {
+    indices,
+    positions,
+    normals: positions,
+    uvs,
+  });
+};
+
+// -----------------------------------------------------------------------------
+// Texture conversion
+
+/**
+ * Create a matrix converting a vector (u, v, 1)
+ * to the corresponding 3D position on an icosahedron face.
+ * 
+ * The conversion is based on a particular face described by the parameters.
+ */
+function uv12pos(
+  u_w: number, v_we: number, v_p: number,
+  radius_p: number, y_we: number, y_p: number,
+) {
+  const u_p = u_w + .1, u_e = u_w + .2;
+  return B.Matrix.GetAsMatrix3x3(
+    createMat3(
+      u_w, v_we, 1,
+      u_p, v_p , 1,
+      u_e, v_we, 1,
+    )
+    .invert()
+    .multiply(createMat3(
+      radius   * Math.cos(u_w * TAU), y_we, radius   * Math.sin(u_w * TAU),
+      radius_p * Math.cos(u_p * TAU), y_p , radius_p * Math.sin(u_p * TAU),
+      radius   * Math.cos(u_e * TAU), y_we, radius   * Math.sin(u_e * TAU),
+    ))
+  );
+}
+
+// When we convert (u,v) coordinates to longitude/latitude,
+// we first shift u so that our point lands in one of the four
+// "reference faces" 0, 5, 14, and 19.
+// We precompute conversion matrices for the reference faces.
+
+//                          u0   v_we      v_p      radius_p  y_we     y_p
+const uv12pos_0  = uv12pos( 0.0, (1-dv)/2,  1-dv   , 0     ,  height,  1     );
+const uv12pos_5  = uv12pos( 0.0, (1-dv)/2,  0      , radius,  height, -height);
+const uv12pos_14 = uv12pos(-0.1, 0       , (1-dv)/2, radius, -height,  height);
+const uv12pos_19 = uv12pos(-0.1, 1       , (1+dv)/2, 0     , -height, -1     );
+
+
+B.Effect.ShadersStore.MyIcoSpriteFragmentShader = `
+varying vec2 vUV;
+
+uniform sampler2D base;
+
+mat3 uv12pos[4] = mat3[4](${
+  [uv12pos_14, uv12pos_5, uv12pos_0, uv12pos_19].map(mat => `
+  mat3(${mat})`).join(",")}
+);
+
+void main(void) {
+  float u = vUV.x, v = vUV.y;
+
+  // divide horizontally in fifths
+  float u5 = u * 5.;
+  float u5fract = fract(u5);
+
+  // are we in the right half of a fifth?
+  bool right = u5fract > .5;
+
+  // divide vertically at (1 - dv)/2
+  bool polar = v > ${(1 - dv) / 2};
+
+  // split at diagonal lines:
+  bool upper = v > mix(
+    mix(${(1 - dv) / 2}, 0.5          , polar),
+    mix(0.             , ${1 - dv / 2}, polar),
+    2. * mix(u5fract, 1. - u5fract, right)
+  );
+
+  // offset (in u direction) between the "reference face" 0, 5, 14 or 19 and
+  // the actual face we are in:
+  float uOffset = (floor(u5) + float(right && (upper == polar))) * .2;
+
+  // 3D position on reference face:
+  vec3 pos = uv12pos[2 * int(polar) + int(upper)] * vec3(u - uOffset, v, 1.);
+
+  gl_FragColor = texture(base, vec2(
+    atan(pos.z, pos.x) * ${1/TAU} + uOffset,
+    atan(pos.y, length(pos.xz)) * ${2/TAU} + .5
+  ));
+}
+`;
+
+/**
+ * Convert an equirectangular sphere texture to a sprite texture appropriate
+ * for icospheres.
+ */
+export const createIcoSprite = (
+  name: string, width: number, base: B.Texture, scene: B.Scene
+): B.Texture =>
+  Object.assign(
+    new B.ProceduralTexture(name, {
+      width,
+      height: Math.round(width * Math.sqrt(3) / (5 * (1 - dv))),
+    }, "MyIcoSprite", scene)
+    .setTexture("base", base),
+    {
+      wrapU: B.Texture.WRAP_ADDRESSMODE,
+      wrapV: B.Texture.WRAP_ADDRESSMODE,
+    }
+  );
---
+++ animate/src/index.ts
@@ -0,0 +1,418 @@
+import * as B from "@babylonjs/core";
+import { createIcoSprite, createIcoVertices, dv } from "./MyIcoSphere";
+
+let nameCount = 0;
+const nm = (base: string) => base + "#" + nameCount++;
+
+// -----------------------------------------------------------------------------
+// Abbreviations and Utilities
+
+const TAU = 2 * Math.PI;
+
+type V3 = B.Vector3;
+const V3 = B.Vector3;
+const v3 = (x: number, y: number, z: number) => new V3(x, y, z);
+
+const createStandardMaterial = (
+  name: string,
+  options: Partial<B.StandardMaterial>,
+  scene?: B.Scene
+): B.StandardMaterial =>
+  Object.assign(new B.StandardMaterial(nm(name), scene), options);
+
+// -----------------------------------------------------------------------------
+// Set up engine/scene/camera/lighting
+
+const canvas = document.querySelector<HTMLCanvasElement>('#renderCanvas');
+const engine = new B.Engine(canvas, true, {
+  preserveDrawingBuffer: true,
+  stencil: true
+});
+
+const scene = new B.Scene(engine);
+scene.clearColor = new B.Color4(0, 0, 0, 0);
+
+const camera = new B.ArcRotateCamera(nm("camera"), .05 * TAU, .2 * TAU, 7, v3(0, 0, 0), scene);
+camera.lowerRadiusLimit = 2.1;
+camera.upperRadiusLimit = 10;
+camera.attachControl(undefined, true);
+
+
+const light = new B.HemisphericLight('light1', v3(0, 1, 0), scene);
+light.intensity = 0.8;
+
+const light2 = new B.DirectionalLight("light2", v3(10, -2, -10), scene);
+light2.intensity = 0.8;
+
+const light3 = new B.DirectionalLight("light3", v3(3, 10, 10), scene);
+light3.intensity = 0.5;
+
+const light4 = new B.DirectionalLight("light4", v3(-10, 3, -3), scene);
+light4.intensity = 0.5;
+
+
+if (false) {
+  ([[1,0,0], [0,1,0], [0,0,1]] as [number, number, number][])
+  .forEach((dims, i) => {
+    const color = new B.Color3(...dims);
+    const arrow = B.MeshBuilder.CreateTube("arrow-" + i, {
+      path: [0, .9, .9, 1].map(s => v3(...dims).scaleInPlace(s)),
+      radiusFunction: i => [.008, .008, .024, 0][i],
+    }, scene);
+    arrow.material = createStandardMaterial("arrowMat", {
+      diffuseColor: color,
+      // emissiveColor: color,
+    }, scene);
+  });
+
+  // Allow to hide some vertices temporarily inside the origin
+  const origin = B.MeshBuilder.CreateIcoSphere("origin", {
+    radius: 0.02,
+  }, scene);
+  origin.material =
+    createStandardMaterial("originMat", {
+      diffuseColor: B.Color3.Black(),
+    }, scene);
+  }
+
+// -----------------------------------------------------------------------------
+
+  const latLonElement = document.querySelector<HTMLInputElement>("#latLon")!;
+  latLonElement.addEventListener("change", setVisibility);
+
+  const latClosednessElem = document.querySelector<HTMLInputElement>("#latClosedness")!;
+  latClosednessElem.addEventListener("input", repositionLL);
+
+  const lonClosednessElem = document.querySelector<HTMLInputElement>("#lonClosedness")!;
+  lonClosednessElem.addEventListener("input", repositionLL);
+
+  const icoSphElement = document.querySelector<HTMLInputElement>("#icosph")!;
+  icoSphElement.addEventListener("change", setVisibility);
+
+  const icoBulgesElement = document.querySelector<HTMLInputElement>("#icoBulges")!;
+  icoBulgesElement?.addEventListener("input", adaptBulge);
+
+  const icosahedronElement = document.querySelector<HTMLInputElement>("#icosahedron")!;
+  icosahedronElement.addEventListener("change", setVisibility);
+
+  const icoClosednessElem = document.querySelector<HTMLInputElement>("#icoClosedness")!;
+  icoClosednessElem.addEventListener("input", adaptIco);
+
+  const icoShiftSouthElem = document.querySelector<HTMLInputElement>("#icoShiftSouth")!;
+  icoShiftSouthElem.addEventListener("input", adaptIco);
+
+// -----------------------------------------------------------------------------
+// Textures/Sprites
+
+// See https://en.wikipedia.org/wiki/File:Blue_Marble_Next_Generation_%2B_topography_%2B_bathymetry.jpg
+// for origin (NASA) and copyright (public domain).
+const url = "https://upload.wikimedia.org/wikipedia/commons/5/56/Blue_Marble_Next_Generation_%2B_topography_%2B_bathymetry.jpg";
+const baseTexture = Object.assign(new B.Texture(url, scene, true), {
+  wrapU: B.Texture.WRAP_ADDRESSMODE,
+  wrapV: B.Texture.CLAMP_ADDRESSMODE,
+});
+
+const backMat = createStandardMaterial("back mat", {
+  diffuseColor : new B.Color3(.5, .5, .5),
+  specularColor: new B.Color3(.5, .5, .5),
+}, scene);
+
+const llMat = createStandardMaterial("latLon sphere mat", {
+  specularColor: new B.Color3(.5, .5, .5),
+  diffuseTexture: baseTexture,
+}, scene);
+
+const icoMat = createStandardMaterial("icosphere mat", {
+  specularColor: new B.Color3(.5, .5, .5),
+  diffuseTexture: createIcoSprite(nm("myIcoSprite"), 3600, baseTexture, scene),
+}, scene);
+
+// -----------------------------------------------------------------------------
+// Lat/Lon Sphere
+
+const llMesh = new B.Mesh(nm("latLon sphere"), scene);
+llMesh.material = llMat;
+
+const llBackMesh = new B.Mesh(nm("latLon sphere back"), scene);
+llBackMesh.material = backMat;
+
+const stepsPerRightAngle = 90;
+
+const nLongitudes = 4 * stepsPerRightAngle + 1;
+const nLatitudes = 2 * stepsPerRightAngle + 1;
+const nVertices = nLatitudes * nLongitudes;
+
+const indices =
+  new Float32Array((4 * stepsPerRightAngle) * (2 * stepsPerRightAngle) * 2 * 3);
+const backIndices =
+  new Float32Array((4 * stepsPerRightAngle) * (2 * stepsPerRightAngle) * 2 * 3);
+{
+  let vtxIdx = 0;
+  let indicesIdx = 0;
+  for (let i = -stepsPerRightAngle; i < stepsPerRightAngle; i++) {
+    for (let j = -2*stepsPerRightAngle; j < 2*stepsPerRightAngle; j++) {
+      const A = vtxIdx++;
+      const B = A + 1;
+      const D = A + (4*stepsPerRightAngle+1);
+      const C = D + 1;
+
+      indices[indicesIdx] = A; backIndices[indicesIdx++] = A;
+      indices[indicesIdx] = B; backIndices[indicesIdx++] = D;
+      indices[indicesIdx] = D; backIndices[indicesIdx++] = B;
+
+      indices[indicesIdx] = B; backIndices[indicesIdx++] = B;
+      indices[indicesIdx] = C; backIndices[indicesIdx++] = D;
+      indices[indicesIdx] = D; backIndices[indicesIdx++] = C;
+    }
+    vtxIdx++;
+  }
+}
+
+const llUVs = new Float32Array(nVertices * 2);
+{
+  let uvIdx = 0;
+  for (let i = -stepsPerRightAngle; i <= stepsPerRightAngle; i++) {
+    for (let j = -2*stepsPerRightAngle; j <= 2*stepsPerRightAngle; j++) {
+      llUVs[uvIdx++] = (j + 2 * stepsPerRightAngle) / (4 * stepsPerRightAngle);
+      llUVs[uvIdx++] = (i + stepsPerRightAngle) / (2 * stepsPerRightAngle);
+    }
+  }
+}
+
+const step = TAU / (4 * stepsPerRightAngle);
+
+const llPos = new Float32Array(nVertices * 3);
+
+const flatLimit = 1e-3;
+
+// TODO implement as vertex shader
+function setLL(latClosedness: number, lonClosedness: number) {
+  const latFlat = latClosedness < flatLimit;
+  const latStep = step * latClosedness;
+  const rLat = 1 / latClosedness;
+
+  const lonFlat = lonClosedness < flatLimit;
+  const lonStep = step * lonClosedness;
+  const rLon = 1 / lonClosedness;
+
+  let posIdx = 0;
+  for (let i = -stepsPerRightAngle; i <= stepsPerRightAngle; i++) {
+    const lat = i * latStep;
+    const y    = latFlat ? i * step : rLat *  Math.sin(lat);
+    const rXZ1 = latFlat ? 0        : rLat * (Math.cos(lat) - 1);
+    const rXZ2 = rLon + rXZ1;
+    for (let j = -2*stepsPerRightAngle; j <= 2*stepsPerRightAngle; j++) {
+      const lon = j * lonStep;
+      llPos[posIdx++] = 1 + (lonFlat ? rXZ1     : rXZ2 * Math.cos(lon) - rLon);
+      llPos[posIdx++] = y;
+      llPos[posIdx++] =      lonFlat ? j * step : rXZ2 * Math.sin(lon);
+    }
+  }
+}
+
+setLL(Number(latClosednessElem.value), Number(lonClosednessElem.value));
+
+const llVertexData = Object.assign(new B.VertexData(), {
+  indices,
+  positions: llPos,
+  uvs: llUVs,
+});
+
+llVertexData.applyToMesh(llMesh, true);
+llMesh.createNormals(true);
+
+const llBackVertexData = Object.assign(new B.VertexData(), {
+  indices: backIndices,
+  positions: llPos,
+});
+
+llBackVertexData.applyToMesh(llBackMesh, true);
+llBackMesh.createNormals(true);
+
+function repositionLL() {
+  setLL(Number(latClosednessElem.value), Number(lonClosednessElem.value));
+  llMesh.updateVerticesData(B.VertexBuffer.PositionKind, llPos);
+  llMesh.createNormals(true);
+  llBackMesh.updateVerticesData(B.VertexBuffer.PositionKind, llPos);
+  llBackMesh.createNormals(true);
+}
+
+// -----------------------------------------------------------------------------
+// Icosphere
+
+const icoSphMesh = new B.Mesh(nm("icosphere"), scene);
+icoSphMesh.material = icoMat;
+
+icoSphMesh.rotate(B.Axis.Y, TAU/2);
+
+const icoSphVertexData = createIcoVertices(12);
+
+icoSphVertexData.applyToMesh(icoSphMesh, true);
+
+function adaptBulge(): void {
+  const bulge = Number(icoBulgesElement.value);
+
+  // TODO
+  // - computing full vtxData is not needed, only positions
+  // - should not create new data structures but write into existing
+  // - might/should go to WebGL
+
+  const {positions} = createIcoVertices(12, bulge);
+  icoSphMesh.updateVerticesData(B.VertexBuffer.PositionKind, positions);
+  if (bulge === 1) {
+    icoSphMesh.updateVerticesData(B.VertexBuffer.NormalKind, positions);
+  } else {
+    icoSphMesh.createNormals(true);
+  }
+}
+
+adaptBulge();
+
+// -----------------------------------------------------------------------------
+// Icosahedron
+
+// TODO use a tree of single-face meshes instead of one multi-face mesh?
+
+const icoMesh = new B.Mesh(nm("icosahedron"), scene);
+icoMesh.material = icoMat;
+
+const icoBackMesh = new B.Mesh(nm("icosahedron back"), scene);
+icoBackMesh.material = backMat;
+
+const namedIndices: Record<string, number> = {};
+"abcdefghijklmnopqrLMNOPQRSTUVWX".split("").forEach((c, i) => namedIndices[c] = i);
+
+const icoIndices = new Float32Array((20 + 2) * 3);
+const icoBackIndices = new Float32Array((20 + 2) * 3);
+
+`
+   fga ghb hic ijd jke
+   fmg gnh hoi ipj jqk
+ lmf mng noh opi pqj qrk
+ LSM MTN NUO OVP PWQ QXR`
+.trim().split(/\s+/).forEach(([c0, c1, c2], f) => {
+  icoBackIndices[3 * f + 0] = icoIndices[3 * f + 0] = namedIndices[c0];
+  icoBackIndices[3 * f + 2] = icoIndices[3 * f + 1] = namedIndices[c1];
+  icoBackIndices[3 * f + 1] = icoIndices[3 * f + 2] = namedIndices[c2];
+});
+
+const icoUVs = Float32Array.of(
+  ...[   0.1,  0.3,  0.5,  0.7,  0.9   ].flatMap(u => [u,  1-dv   ]), // a-e
+  ...[0.0,  0.2,  0.4,  0.6,  0.8,  1.0].flatMap(u => [u, (1-dv)/2]), // f-k
+  ...[0, 0.1,  0.3,  0.5,  0.7,  0.9, 1].flatMap(u => [u,  0      ]), // l-r
+  ...[0, 0.1,  0.3,  0.5,  0.7,  0.9, 1].flatMap(u => [u,  1      ]), // L-R
+  ...[0.0,  0.2,  0.4,  0.6,  0.8,  1.0].flatMap(u => [u, (1+dv)/2]), // S-X
+);
+
+/** How far above/below the equator are the non-pole vertices? */
+const height = Math.sqrt(1 / 5);
+/** How far away from the main axis are the non-pole vertices? */
+const radius = 2 * height;
+const dihedralAngle = Math.acos(-Math.sqrt(5)/3); // ~ 138.2°
+/**
+ * What fraction of a right angle must be subtracted from a straight angle
+ * to get the dihedral angle? 
+ */
+const anglePart = 2 - dihedralAngle / (TAU / 4);
+
+const icoPos = new Float32Array(31*3);
+
+function fillIcoPos(bend: number, shiftSouth: number) {
+  function nextVertex(a: V3, b: V3, c: V3) {
+    const mid_ab = a.add(b).scaleInPlace(.5);
+    const height = mid_ab.subtract(c);
+    const normal = a.subtract(c).cross(b.subtract(c)).normalize().scale(height.length());
+    const rotated = V3.SlerpToRef(height, normal, bend * anglePart, new V3());
+    return mid_ab.add(rotated);
+  }
+
+  // TODO "hoi" should rotate according to bend
+  const mid_hi = v3(radius * Math.cos(TAU/10), height, 0);
+  const oHeight = v3(radius, -height, 0).subtract(mid_hi);
+  const oHeightFlat = v3(0, -oHeight.length(), 0);
+
+  const h_ = v3(radius * Math.cos(-TAU/10),  height, radius * Math.sin(-TAU/10));
+  const o_ = mid_hi.add(V3.SlerpToRef(oHeightFlat, oHeight, bend, new V3()));
+  const i_ = v3(radius * Math.cos( TAU/10),  height, radius * Math.sin( TAU/10));
+
+  const n_ = nextVertex(h_, o_, i_);
+  const g_ = nextVertex(h_, n_, o_);
+  const m_ = nextVertex(g_, n_, h_);
+  const f_ = nextVertex(g_, m_, n_);
+  const lAux = nextVertex(f_, m_, g_);
+  const l_ = m_.add(lAux).scale(0.5);
+
+  const p_ = nextVertex(o_, i_, h_);
+  const j_ = nextVertex(p_, i_, o_);
+  const q_ = nextVertex(p_, j_, i_);
+  const k_ = nextVertex(q_, j_, p_);
+  const rAux = nextVertex(q_, k_, j_);
+  const r_ = q_.add(rAux).scale(0.5);
+
+  const a_ = nextVertex(g_, f_, m_);
+  const b_ = nextVertex(h_, g_, n_);
+  const c_ = nextVertex(i_, h_, o_);
+  const d_ = nextVertex(j_, i_, p_);
+  const e_ = nextVertex(k_, j_, q_);
+
+  const s_ = nextVertex(lAux, m_, f_);
+  const t_ = nextVertex(m_, n_, g_);
+  const u_ = nextVertex(n_, o_, h_);
+  const v_ = nextVertex(o_, p_, i_);
+  const w_ = nextVertex(p_, q_, j_);
+  const x_ = nextVertex(q_, rAux, k_);
+
+  const shiftX = shiftSouth * (1 - shiftSouth);
+  const shiftY = (c_.y - o_.y + .02) * shiftSouth;
+
+  [
+      a_, b_, c_, d_, e_,
+   f_,  g_, h_, i_, j_,  k_,
+   l_,m_, n_, o_, p_, q_,r_,
+   l_,m_, n_, o_, p_, q_,r_,
+   s_,  t_, u_, v_, w_,  x_,
+  ].forEach(({x, y, z}, i) => {
+    icoPos[3 * i + 0] = x + (i > 17 ? shiftX : 0);
+    icoPos[3 * i + 1] = y + (i > 17 ? shiftY : 0);
+    icoPos[3 * i + 2] = z;
+  });
+}
+
+fillIcoPos(Number(icoClosednessElem.value), Number(icoShiftSouthElem.value));
+
+const icoVertexData = Object.assign(new B.VertexData(), {
+  indices: icoIndices,
+  positions: icoPos,
+  uvs: icoUVs,
+});
+
+const icoBackVertexData = Object.assign(new B.VertexData(), {
+  indices: icoBackIndices,
+  positions: icoPos,
+  uvs: icoUVs,
+});
+
+icoVertexData.applyToMesh(icoMesh, true);
+icoBackVertexData.applyToMesh(icoBackMesh, true);
+
+function adaptIco() {
+  fillIcoPos(Number(icoClosednessElem.value), Number(icoShiftSouthElem.value));
+  icoMesh.updateVerticesData(B.VertexBuffer.PositionKind, icoPos);
+  icoBackMesh.updateVerticesData(B.VertexBuffer.PositionKind, icoPos);
+}
+
+// -----------------------------------------------------------------------------
+
+function setVisibility() {
+  llMesh.isVisible = llBackMesh.isVisible = latLonElement.checked;
+  icoSphMesh.isVisible = icoSphElement.checked;
+  icoMesh.isVisible = icoBackMesh.isVisible = icosahedronElement.checked;
+}
+
+setVisibility();
+
+// -----------------------------------------------------------------------------
+
+engine.runRenderLoop(() => scene.render());
+
+window.addEventListener('resize', () => engine.resize());
